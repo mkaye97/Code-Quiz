bb39282b3df65853da5f24c2a22ce8190c53e75a
mkaye <mattkaye97@gmail.com>
2022-12-11 22:48:15 -0500

    Added object holding all questions and answers. Made minor adjustments to index.html and style.css.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,17 +2,62 @@ var timeRemaining = $('#timer');
 var startButton = $('#startbutton');
 var highScore = $('#highscore');
 var card = $('#card');
+var questionLines = $('#qLines');
+var textLines = $('#line')
+var nextButton = $('#next');
+
+
+
+var questions = [{
+  question: '1) Which of these, is not a common data type?',
+  answers:['a. Boolean', 'b. Number', 'c. Color', 'd. String'], 
+  correctAnswer: 'c. Color'
+}, {
+  question: '2) When using jQuery, what symbol is used to denote a "selector"?',
+  answers:['a. Boolean', 'b. Number', 'c. Color', 'd. String'], 
+  correctAnswer: 'c. Color'
+}, {
+  question: '3) What does "API" stand for?',
+  answers:['a. Boolean', 'b. Number', 'c. Color', 'd. String'], 
+  correctAnswer: 'c. Color'
+}, {
+  question: '4) What is the purpose of using a CSS reset?',
+  answers:['a. Boolean', 'b. Number', 'c. Color', 'd. String'], 
+  correctAnswer: 'c. Color'
+}, {
+  question: '5) Which of these is not a semantic HTML element?',
+  answers:['a. Boolean', 'b. Number', 'c. Color', 'd. String'], 
+  correctAnswer: 'c. Color'
+}, {
+  question: '6) Which coding language focuses on application/page functionality?',
+  answers:['a. Boolean', 'b. Number', 'c. Color', 'd. String'], 
+  correctAnswer: 'c. Color'
+}, {
+  question: '7) What does the terminal command "mkdir HTML do?',
+  answers:['a. Boolean', 'b. Number', 'c. Color', 'd. String'], 
+  correctAnswer: 'c. Color'
+}, {
+  question: '8) What does the "flex" property do in CSS?',
+  answers:['a. Boolean', 'b. Number', 'c. Color', 'd. String'], 
+  correctAnswer: 'c. Color'
+}, {
+  question: '9) Which of these symbols denotes a class in CSS?',
+  answers:['a. Boolean', 'b. Number', 'c. Color', 'd. String'], 
+  correctAnswer: 'c. Color'
+}, {
+  question: '10) What is the terminal command to find your current working directory?',
+  answers:['a. Boolean', 'b. Number', 'c. Color', 'd. String'], 
+  correctAnswer: 'c. Color'
+}];
+
+var points = 0;
 
 function startQuiz() {
     // Removes 'Start' and 'High Score' buttons and starts timer.
-    startButton.remove();
-    highScore.remove();
     $('#initialbuttons').remove();
     setTime();
-    // Need to insert h4 element and append question text.
-    // card.add("h4");
-    // $('#q').text(questions[0]);
- 
+    $('#qlist').removeClass("d-none");
+    quiz();
 }
 
 // Creates timer for quiz and counts down every second (1000ms).
@@ -29,12 +74,12 @@ function setTime() {
   
     }, 1000)};
 
-function nextQuestion() {
+function quiz() {
 
 }
 
 function selectAnswer() {
 
-}
+};
 
 startButton.on('click', startQuiz);
